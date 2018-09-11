ae2ac0727c9b746c8b29c5f2e6b36b8280dc6eb4
Kjay1181 <42454194+Kjay1181@users.noreply.github.com>
2018-09-11 13:03:54 -1000

    Add files via upload

---
+++ burger.js
@@ -0,0 +1,14 @@
+var hamburger={
+        name:"Kams famous buns",
+        bun:["Plain","Sesame","whole wheat"],
+        cheese:["american","mozzarella","bleu cheese"],
+        meat:["pork","beef","chicken"],
+        veg:["pickles","lettuce","cucumber","tomatoes"],
+        mustard:true,
+        ketchup:true,
+        mayo:true,
+        price:10,
+        ounces:3
+        
+      }
+      
